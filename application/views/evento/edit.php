00310992283801bf04ae03916883570e5025d674
Christian Jara <christian.jara@inmater.pe>
2024-06-05 20:48:25 -0500

    validacion de campos

--- application/views/evento/edit.php
+++ application/views/evento/edit.php
@@ -18,20 +18,20 @@
 
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Titulo</label>
-                        <input type="text" class="form-control" id="titulo" name="titulo" value="<?php echo $eventos->titulo;?>"  placeholder="Ingresa titulo">
+                        <input type="text" class="form-control" id="titulo" name="titulo" value="<?php echo $eventos->titulo;?>"  placeholder="Ingresa titulo" required>
                     </div>
                     <div class="mb-3">
                         <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
-                        <textarea class="form-control" name="descripcion" rows="3"><?php echo $eventos->descripcion;?></textarea>
+                        <textarea class="form-control" name="descripcion" rows="3" required><?php echo $eventos->descripcion;?></textarea>
                     </div>
                     <div class="row">
                         <div class="col-md-6 mb-3">
                             <label for="fec_inicio" class="form-label">Inicio</label>
-                            <input type="datetime-local" class="form-control" id="fec_inicio" name="fec_inicio" value="<?php echo $eventos->fecha_inicio;?>">
+                            <input type="datetime-local" class="form-control" id="fec_inicio" name="fec_inicio" value="<?php echo $eventos->fecha_inicio;?>" required>
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="fec_fin" class="form-label">Fin</label>
-                            <input type="datetime-local" class="form-control" id="fec_fin" name="fec_fin" value="<?php echo $eventos->fecha_fin;?>">
+                            <input type="datetime-local" class="form-control" id="fec_fin" name="fec_fin" value="<?php echo $eventos->fecha_fin;?>" required>
                         </div>
                     </div>
                     <button type="submit" class="btn btn-primary">Actualizar</button>
--- application/views/evento/index.php
+++ application/views/evento/index.php
@@ -16,20 +16,20 @@
                 <form id="evento" name="evento" method="Post" action="<?php echo base_url(); ?>index.php/evento/registrar">
                     <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Titulo</label>
-                        <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Ingresa titulo">
+                        <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Ingresa titulo" required>
                     </div>
                     <div class="mb-3">
                         <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
-                        <textarea class="form-control" id="descripcion" name="descripcion" rows="3"></textarea>
+                        <textarea class="form-control" id="descripcion" name="descripcion" rows="3" required></textarea>
                     </div>
                     <div class="row">
                         <div class="col-md-6 mb-3">
                             <label for="fec_inicio" class="form-label">Inicio</label>
-                            <input type="datetime-local" class="form-control" id="fec_inicio" name="fec_inicio">
+                            <input type="datetime-local" class="form-control" id="fec_inicio" name="fec_inicio" required>
                         </div>
                         <div class="col-md-6 mb-3">
                             <label for="fec_fin" class="form-label">Fin</label>
-                            <input type="datetime-local" class="form-control" id="fec_fin" name="fec_fin">
+                            <input type="datetime-local" class="form-control" id="fec_fin" name="fec_fin" required>
                         </div>
                     </div>
                     <button type="submit" class="btn btn-primary">Guardar</button>
